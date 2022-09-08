950650b14b785493651fc25e865af2bbcbb7fb42
B-usman <babatundeusman82@gmail.com>
2022-09-08 07:54:30 +0100

    page uploaded

---
+++ main.js
@@ -0,0 +1,118 @@
+
+
+document.getElementById('view').onclick = function(){
+    document.getElementById('res').style.display = "block";
+
+}
+
+
+document.getElementById('opt2').ondblclick = function(){
+    document.getElementById('q1').style.display = "none";
+    document.getElementById('q2').style.display = "block";
+}
+
+document.getElementById('opt3').ondblclick = function(){
+    document.getElementById('q1').style.display = "none";
+    document.getElementById('q2').style.display = "block";
+}
+
+document.getElementById('opt4').ondblclick = function(){
+    document.getElementById('q1').style.display = "none";
+    document.getElementById('q2').style.display = "block";
+}
+
+
+// question 2
+document.getElementById('opt1-q2').ondblclick = function(){
+    document.getElementById('q1').style.display = "none";
+    document.getElementById('q2').style.display = "none";
+    document.getElementById('q3').style.display = "block";
+
+    document.getElementById('sc').textContent = score + 10;
+}
+
+document.getElementById('opt2-q2').ondblclick = function(){
+    document.getElementById('q1').style.display = "none";
+    document.getElementById('q2').style.display = "none";
+    document.getElementById('q3').style.display = "block";
+
+    
+}
+
+document.getElementById('opt3-q2').ondblclick = function(){
+    document.getElementById('q1').style.display = "none";
+    document.getElementById('q2').style.display = "none";
+    document.getElementById('q3').style.display = "block";  
+}
+
+document.getElementById('opt4-q2').ondblclick = function(){
+    document.getElementById('q1').style.display = "none";
+    document.getElementById('q2').style.display = "none";
+    document.getElementById('q3').style.display = "block";  
+}
+
+
+// question 3
+document.getElementById('opt1-q3').ondblclick = function(){
+    document.getElementById('q1').style.display = "none";
+    document.getElementById('q2').style.display = "none";
+    document.getElementById('q3').style.display = "none";
+    document.getElementById('q4').style.display = "block";   
+}
+
+document.getElementById('opt2-q3').ondblclick = function(){
+    document.getElementById('q1').style.display = "none";
+    document.getElementById('q2').style.display = "none";
+    document.getElementById('q3').style.display = "none";
+    document.getElementById('q4').style.display = "block";  
+}
+
+document.getElementById('opt3-q3').ondblclick = function(){
+    document.getElementById('q1').style.display = "none";
+    document.getElementById('q2').style.display = "none";
+    document.getElementById('q3').style.display = "none";
+    document.getElementById('q4').style.display = "block";  
+}
+
+document.getElementById('opt4-q3').ondblclick = function(){
+    document.getElementById('q1').style.display = "none";
+    document.getElementById('q2').style.display = "none";
+    document.getElementById('q3').style.display = "none";
+    document.getElementById('q4').style.display = "block";  
+}
+
+
+// question 4
+document.getElementById('opt1-q4').ondblclick = function(){
+    document.getElementById('q1').style.display = "none";
+    document.getElementById('q2').style.display = "none";
+    document.getElementById('q3').style.display = "none";
+    document.getElementById('q4').style.display = "none";
+    document.getElementById('cmp').style.display = "block";
+}
+
+document.getElementById('opt2-q4').ondblclick = function(){
+    document.getElementById('q1').style.display = "none";
+    document.getElementById('q2').style.display = "none";
+    document.getElementById('q3').style.display = "none";
+    document.getElementById('q4').style.display = "none"; 
+    document.getElementById('cmp').style.display = "block";
+}
+
+document.getElementById('opt3-q4').ondblclick = function(){
+    document.getElementById('q1').style.display = "none";
+    document.getElementById('q2').style.display = "none";
+    document.getElementById('q3').style.display = "none";
+    document.getElementById('q4').style.display = "none";
+    document.getElementById('cmp').style.display = "block";
+}
+
+document.getElementById('opt4-q4').ondblclick = function(){
+    document.getElementById('q1').style.display = "none";
+    document.getElementById('q2').style.display = "none";
+    document.getElementById('q3').style.display = "none";
+    document.getElementById('q4').style.display = "none";
+    document.getElementById('cmp').style.display = "block";
+}
+
+// result
